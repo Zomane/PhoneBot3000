514ce498eaa4119c0250e718090ad6afc156c8b9
Azamatik159 <58002957+Azamat1k159@users.noreply.github.com>
2023-06-30 21:14:30 +0300

    Update Search_script.py

--- Search_script.py
+++ Search_script.py
@@ -1,31 +1,14 @@
 class Search:
-    def __init__(self, product: list[str], plants: list[str]):
+    def __init__(self, mass: list[str]):
         """
         --- ЭТО ИНИТ ---
         !! ОН ВЫПОЛНЯЕТСЯ ПРИ ОБЪЯВЛЕНИИ КЛАССА И НИЧЕГО НЕ ВОЗРАЩАЕТ !!
         Получает массив с продуктами и массив с растениями
         *В массивах строки*
         """
-        self.product = set(product)
-        self.plants = set(plants)
+        self.value_mass = set(mass)
 
-    def search_new(self, massage: str) -> list[set[str]]:
-        """
-        --- ЭТО ПОИСК ВСЕХ ВХОЖДЕНИЙ ВСЕГО ---
-        Функция ищет все вхождения в строку как продуктов так и растений
-        Принемает сроку сообщения
-        Возвращает массив с сетами, в сетах строки
-        """
-
-        context_product, context_plant = [], []
-        for item in massage.split(" "):
-            if self.product.__contains__(item):
-                context_product.append(item)
-            elif self.plants.__contains__(item):
-                context_plant.append(item)
-        return [set(context_product), set(context_plant)]
-
-    def search_product_many(self, massage: str) -> set[str]:
+    def search(self, massage: str) -> set[str]:
         """
         --- ЭТО ПОИСК ВСЕХ ВХОЖДЕНИЙ ПРОДУКТА ---
         Функция ищет все вхождения продуктов строку
@@ -34,11 +17,11 @@ class Search:
         """
         context_product = []
         for item in massage.split(" "):
-            if self.product.__contains__(item):
+            if self.value_mass.__contains__(item):
                 context_product.append(item)
         return set(context_product)
 
-    def search_product_gen(self, massage: str) -> set[str]:
+    def search_gen(self, massage: str) -> set[str]:
         """
         --- ЭТО ПОИСК ПРОДУКТА (ГЕНЕРАТОР) ---
         Функция ищет все вхождения продуктов в строку
@@ -48,71 +31,39 @@ class Search:
         """
 
         for item in massage.split(" "):
-            if self.product.__contains__(item):
-                yield item
-
-    def search_plant_many(self, massage: str) -> list[set[str]]:
-        """
-        --- ЭТО ПОИСК ВСЕХ ВХОЖДЕНИЙ РАСТЕНИЙ ---
-        Функция ищет все вхождения растений строку
-        Принемает сроку сообщения
-        Возвращает set с массивом, в массиве строки
-        """
-
-        context_plant = []
-        for item in massage.split(" "):
-            if self.product.__contains__(item):
-                context_plant.append(item)
-        return set(context_plant)
-
-    def search_plant_gen(self, massage: str) -> set[str]:
-        """
-        --- ЭТО ПОИСК РАСТЕНИЙ (ГЕНЕРАТОР) ---
-        Функция ищет все вхождения астений в строку
-        Принемает сроку сообщения
-        Возвращает по строчно
-        !!! ЭТО ГЕНИРАТОР !!!
-        """
-
-        for item in massage.split(" "):
-            if self.product.__contains__(item):
+            if self.value_mass.__contains__(item):
                 yield item
 
 
 if __name__ == "__main__":
     """ это нужно для теста класса """
 
-    mass1 = ['geg', 'kek', 'lol', 'lolchik', 'hohli', 'ghidi']
-    mass2 = ['fafh', 'kakh', 'paph', 'acab', 'nazi', 'nasvai']
-    msgs = ['Покажи им кто тут жид', 'geg in doma', 'keks ne doma', 'falk ne pon', 'acab reallino', 'ac hihih ne nu a cho ia kek and lol']
-    test_class = Search(mass1, mass2)
+    mass1 = ['geg', 'kek', 'lol', 'lolchik', 'hohli', 'ghidi', 'pops', 'opps', 'lols', 'fals', 'jji']
+    mass2 = ['fafh', 'kakh', 'paph', 'acab', 'nazi', 'nasvai', 'kkk', 'lll', 'errrno', 'lokj']
+    msgs = ['Покажи им кто тут жид', 'geg in doma', 'keks ne doma', 'falk ne pon', 'acab reallino',
+            'ac hihih ne nu a cho ia kek and lol',
+            'ac hihih ne nu a cho and lol', 'ac hihih ne nu a cho and lol', ' ac hihih ne nu a cho and lol']
+    test_class = Search(mass1)
+    test_class2 = Search(mass2)
 
-    print('Тест функции search_new')
-    for i in msgs:
-        print(test_class.search_new(i))
-
-    print('Тест функции search_plant_gen')
+    print('Тест функции search')
     for i in msgs:
         try:
-            print(next(test_class.search_plant_gen(i)))
+            print(next(test_class.search_gen(i)))
         except:
             print('Конец генератора')
 
-    print('Тест функции search_plant_many')
+    print('Тест функции search')
     for i in msgs:
-        print(str(test_class.search_plant_many(i)))
+        print(str(test_class.search(i)))
 
-    print('Тест функции search_product_gen')
-    try:
-        print(next(test_class.search_product_gen(i)))
-    except:
-        print('Конец генератора')
-
-    print('Тест функции search_product_many')
+    print('Тест функции search_gen')
     for i in msgs:
-        print(test_class.search_product_many(i))
+        try:
+            print(next(test_class2.search_gen(i)))
+        except:
+            print('Конец генератора')
 
-
-
-
-
+    print('Тест функции search')
+    for i in msgs:
+        print(test_class2.search(i))
