d6649a9a0226e2d60587a0372d7fbdb7fe2356d1
Azamatik159 <58002957+Azamat1k159@users.noreply.github.com>
2023-06-30 21:43:15 +0300

    Update Search_script.py

--- Search_script.py
+++ Search_script.py
@@ -33,37 +33,3 @@ class Search:
         for item in massage.split(" "):
             if self.value_mass.__contains__(item):
                 yield item
-
-
-if __name__ == "__main__":
-    """ это нужно для теста класса """
-
-    mass1 = ['geg', 'kek', 'lol', 'lolchik', 'hohli', 'ghidi', 'pops', 'opps', 'lols', 'fals', 'jji']
-    mass2 = ['fafh', 'kakh', 'paph', 'acab', 'nazi', 'nasvai', 'kkk', 'lll', 'errrno', 'lokj']
-    msgs = ['Покажи им кто тут жид', 'geg in doma', 'keks ne doma', 'falk ne pon', 'acab reallino',
-            'ac hihih ne nu a cho ia kek and lol',
-            'ac hihih ne nu a cho and lol', 'ac hihih ne nu a cho and lol', ' ac hihih ne nu a cho and lol']
-    test_class = Search(mass1)
-    test_class2 = Search(mass2)
-
-    print('Тест функции search')
-    for i in msgs:
-        try:
-            print(next(test_class.search_gen(i)))
-        except:
-            print('Конец генератора')
-
-    print('Тест функции search')
-    for i in msgs:
-        print(str(test_class.search(i)))
-
-    print('Тест функции search_gen')
-    for i in msgs:
-        try:
-            print(next(test_class2.search_gen(i)))
-        except:
-            print('Конец генератора')
-
-    print('Тест функции search')
-    for i in msgs:
-        print(test_class2.search(i))
