599c84f6c21537f64ed9369ef7b768228bb86b27
Azamatik159 <58002957+Azamat1k159@users.noreply.github.com>
2023-06-30 11:14:41 +0300

    Update classificator_of_contex.py

--- classificator_of_contex.py
+++ classificator_of_contex.py
@@ -3,6 +3,8 @@ import spacy
 
 if __name__ == '__main__':
    nlp = spacy.blank('ru').from_disk('/ru2_combined_400ks_96')
+   import ru2_combined_400ks_96
+   nlp = ru2_combined_400ks_96.load()
    spacy.explain("Degree")
    doc = nlp("Здраствуйте, я хочу купить 'Парацетамол', Россия, Москва")
    for word in doc:
